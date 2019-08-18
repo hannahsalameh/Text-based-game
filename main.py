c1354f0a0920952391859ec000ee75a88e837d3f
Hannah Salameh <hannahsalameh18@gmail.com>
2019-08-17 17:02:15 -0700

    Finished About Me page basically

--- main.py
+++ main.py
@@ -18,7 +18,19 @@ class GamePage(webapp2.RequestHandler):
         game_template = the_jinja_env.get_template('templates/game.html')
         self.response.write(game_template.render())
 
+class AboutMePage(webapp2.RequestHandler):
+    def get(self):
+        aboutme_template = the_jinja_env.get_template('templates/aboutme.html')
+        self.response.write(aboutme_template.render())
+
+class PlayPage(webapp2.RequestHandler):
+    def get(self):
+        play_template = the_jinja_env.get_template('templates/howtoplay.html')
+        self.response.write(play_template.render())
+
 app = webapp2.WSGIApplication([
     ('/', HomePage),
     ('/game', GamePage),
+    ('/aboutme',AboutMePage),
+    ('/howtoplay',PlayPage),
 ], debug=True)
