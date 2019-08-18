58a948ddcd6c7c080c89b67181ee76a550a8a5ac
Hannah Salameh <hannahsalameh18@gmail.com>
2019-08-18 03:00:19 -0700

    got typing effect to work!!!

--- scripts/game.js
+++ scripts/game.js
@@ -8,10 +8,11 @@ function check(){
   editedInput = userInput.toLowerCase();
   if (editedInput == "help"){
       let helpContent = "> help? help yourself!";
-      let text = document.createElement('p');
-      text.setAttribute('class','gameTextGame');
-      text.textContent = helpContent;
-      textHolder.appendChild(text);
+      createEvent(helpContent,textHolder);
+  }
+  else{
+    let confusedText = "> I didn't get that,  could you try something else?";
+    createEvent(confusedText, textHolder);
   }
 }
 
@@ -20,3 +21,41 @@ function handleForm(event) {
   event.preventDefault();
 }
 form.addEventListener('submit', handleForm);
+
+function createEvent(txt,holder){
+  let text = document.createElement('p');
+  text.setAttribute('class','gameTextGame');
+  text.innerHTML = txt;
+  holder.appendChild(text);
+  typewriter = setupTypewriter(text);
+  typewriter.type();
+}
+
+
+function setupTypewriter(t) {
+  var HTML = t.textContent;
+  t.textContent = "";
+  var cursorPosition = 0,
+    tag = "",
+    writingTag = false,
+    tagOpen = false,
+    typeSpeed = 100,
+    tempTypeSpeed = 0;
+    //end of setting up typewriter
+
+  var type = function() {
+    if (HTML[cursorPosition] === " ") {
+      tempTypeSpeed = 0;
+    } else {
+      tempTypeSpeed = Math.random() * typeSpeed + 50;
+    }
+    t.innerHTML += HTML[cursorPosition];
+    cursorPosition += 1;
+    if (cursorPosition < HTML.length) {
+      setTimeout(type, tempTypeSpeed);
+    }
+  };
+  return {
+    type: type
+  };
+}
