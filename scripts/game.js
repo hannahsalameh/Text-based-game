f07484884a5592aca133664d26d089c3f4fa6780
Hannah Salameh <hannahsalameh18@gmail.com>
2019-08-18 21:56:19 -0700

    deleted console logs

--- scripts/game.js
+++ scripts/game.js
@@ -9,16 +9,28 @@ let foundEndings = 0;
 
 //immediate text that happens
 window.onload = function() {
-  let userInput = document.getElementById('gameInput').value;
-  let textHolder = document.getElementsByClassName('gameScroll')[0];
-  let firstP = "> Hello? Is anybody there?";
-  createEvent(firstP,textHolder);
-  let secondP = "> My name is Carol Davis. Im 23. I don't know where I am or how I got here, but I think I've been kidnapped."
-  setTimeout(function(){createEvent(secondP, textHolder)},3000);
-  let thirdP = "> I'm so scared, its so dark..."
-  setTimeout(function(){createEvent(thirdP, textHolder),wait = true},12000);
-  let fourthP = "> Can you help me? Please?"
-  setTimeout(function(){createEvent(fourthP, textHolder),wait = true},15000);
+  if (wait == false) {
+    let userInput = document.getElementById('gameInput').value;
+    let textHolder = document.getElementsByClassName('gameScroll')[0];
+    let firstP = "> Hello? Is anybody there?";
+    createEvent(firstP,textHolder);
+    setTimeout(function(){
+      if (wait ==false){
+        let secondP = "> My name is Carol Davis. Im 23. I don't know where I am or how I got here, but I think I've been kidnapped."
+        createEvent(secondP, textHolder);
+      }},3000);
+    setTimeout(function(){
+      if (wait==false){
+        let thirdP = "> I'm so scared, its so dark..."
+        createEvent(thirdP, textHolder);
+      }},12000);
+    setTimeout(function(){
+      if(wait==false){
+        let fourthP = "> Can you help me? Please?"
+        createEvent(fourthP, textHolder)
+        wait = true;
+      }},15000);
+  }
 }
 
 //handles all inputted text
@@ -35,7 +47,7 @@ function check(){
     textHolder.appendChild(userText);
     editedInput = userInput.toLowerCase();
     document.getElementById('gameInput').value = "";
-    if (editedInput == "hello" || editedInput == "hello!" || editedInput == "hey"){
+    if (editedInput == "hello" || editedInput == "hello!" || editedInput == "hey" || editedInput == "hello?"){
       let greeting = "> Oh thank god someone's here! can you help me out?";
       let greeting2 = "> You have the same number as my mom used to. Thats the only number I can remember, so sorry for bothering you";
       createEvent(greeting,textHolder);
@@ -49,15 +61,17 @@ function check(){
       setTimeout(function(){createEvent(yesText2,textHolder),wait = true},4000);
     }
     else if (editedInput == "no"|| editedInput == "nah" || editedInput == "nope"){
-      let noText = "> No? Really? Ok..."
-      let noText2 = "> I guess you have more important things to do..."
-      let noText3 ="> I guess this is goodbye :("
-      let endingText = "> Type restart to start this story again or type back to go back to the title screen."
-      createEvent(noText,textHolder);
-      setTimeout(function(){createEvent(noText2,textHolder),wait = true},3000);
-      setTimeout(function(){createEvent(noText3,textHolder),wait = true},7000);
-      setTimeout(function(){displayEnding(foundEndings,textHolder),wait = true},9000);
-      setTimeout(function(){createEventMotion(endingText,textHolder),wait = true},11000);
+      if (helpResponse == false){
+        let noText = "> No? Really? Ok..."
+        let noText2 = "> I guess you have more important things to do..."
+        let noText3 ="> I guess this is goodbye :("
+        let endingText = "> Type restart to start this story again or type back to go back to the title screen."
+        createEvent(noText,textHolder);
+        setTimeout(function(){createEvent(noText2,textHolder),wait = true},3000);
+        setTimeout(function(){createEvent(noText3,textHolder),wait = true},7000);
+        setTimeout(function(){displayEnding(foundEndings,textHolder),wait = true},9000);
+        setTimeout(function(){createEventMotion(endingText,textHolder),wait = true},11000);
+      }
     }
     else if(editedInput == "help"){
         let helpContent = "> help? help yourself!";
@@ -77,7 +91,20 @@ function check(){
     }
   }
   else{
-    document.getElementById('gameInput').value = "";
+    let userInput = document.getElementById('gameInput').value;
+    let textHolder = document.getElementsByClassName('gameScroll')[0];
+    let editedInput = userInput.toLowerCase();
+    if (editedInput == "skip"){
+      //displays the skip
+      let userText = document.createElement('p');
+      userText.setAttribute('class','gameTextU');
+      userText.textContent = "> "+ userInput;
+      textHolder.appendChild(userText);
+      wait = true;
+    }
+    else{
+      document.getElementById('gameInput').value = "";
+    }
   }
 }
 
