9f4da1811a4b3ae339033d55d104a859f5937dd6
Hannah Salameh <hannahsalameh18@gmail.com>
2019-09-14 16:09:04 -0400

    added more plotline, removed random text generation

--- scripts/game.js
+++ scripts/game.js
@@ -6,6 +6,8 @@ function handleForm(event) {
 form.addEventListener('submit', handleForm);
 let wait = false;
 let foundEndings = 0;
+let helpResponse = false;
+let feelAround = true;
 
 //immediate text that happens
 window.onload = onloadText();
@@ -25,7 +27,7 @@ function onloadText(){
       if (wait==false){
         let thirdP = "> I'm so scared, its so dark..."
         createEvent(thirdP, textHolder);
-      }},12000);
+      }},13000);
     setTimeout(function(){
       if(wait==false){
         let fourthP = "> Can you help me? Please?"
@@ -47,36 +49,36 @@ function check(){
     textHolder.appendChild(userText);
     editedInput = userInput.toLowerCase();
     document.getElementById('gameInput').value = "";
+    //main story line is written first
     if (editedInput == "hello" || editedInput == "hello!" || editedInput == "hey" || editedInput == "hello?"){
-      let greeting = "> Oh thank god someone's here! can you help me out?";
+      let greeting = "> Oh thank god someone's here! Can you help me out?";
       let greeting2 = "> You have the same number as my mom used to. Thats the only number I can remember, so I'm sorry for bothering you";
       createEvent(greeting,textHolder);
       setTimeout(function(){createEvent(greeting2,textHolder),wait = true},5000);
     }
     else if (editedInput == "yes"||editedInput == "yeah" || editedInput == "yea" || editedInput == "sure"){
       let yesText = "> Thank god! I was so worried you wouldn't respond";
-      let yesText2 = "> Any advice on what to do rn?"
+      let yesText2 = "> Any advice on what to do rn?";
+      helpResponse = true;
       createEvent(yesText,textHolder);
-      setTimeout(function(){createEvent(yesText2,textHolder),wait = true},4000);
+      setTimeout(function(){createEvent(yesText2,textHolder),wait = true},5000);
     }
+    // below this is what happens if they say no to first question
     else if (editedInput == "no"|| editedInput == "nah" || editedInput == "nope"){
       let allGameText = document.getElementsByClassName('gameTextGame');
       let i;
-      let helpResponse = false;
-      for (text of allGameText){
-        if (text.textContent == "> Any advice on what to do rn?"){
-          helpResponse = true;
-        }
-      }
       if (helpResponse == false){
-        let noText = "> No? Really? Ok..."
-        let noText2 = "> I guess you have more important things to do..."
-        let noText3 ="> I guess this is goodbye :("
-        let endingText = "> Type restart to start this story again or type back to go back to the title screen."
+        let noText = "> No? Really? Ok...";
+        let noText2 = "> I guess you have more important things to do...";
+        let noText3 ="> I guess this is goodbye :(";
+        let endingText = "> Type restart to start this story again or type back to go back to the title screen.";
         createEvent(noText,textHolder);
         setTimeout(function(){createEvent(noText2,textHolder)},3000);
         setTimeout(function(){createEvent(noText3,textHolder)},7000);
         setTimeout(function(){displayEnding(foundEndings,textHolder)},10000);
+        if (foundEndings <= 5) {
+          foundEndings += 1;
+        }
         setTimeout(function(){createEventMotion(endingText,textHolder)},11000);
       }
       else{
@@ -84,6 +86,48 @@ function check(){
         createEvent(noTextAlt,textHolder);
       }
     }
+    else if(helpResponse == true && editedInput == "feel around"){
+      let feelText = "> Ok, give me a second..."
+      let feelText2 = "> Oh, so I think there is some stuff on the floor down here..."
+      let feelText3 = "> There's a can, some water, umm.. wait, I think this is my wallet!"
+      let foundText = " Found: Can of Beans, Water, Wallet"
+      feelAround = true;
+      createEvent(feelText,textHolder);
+      setTimeout(function(){createEvent(feelText2,textHolder)},3000);
+      setTimeout(function(){createEvent(feelText3,textHolder)},8500);
+      setTimeout(function(){createEventMotion(foundText,textHolder)},14000);
+    }
+    else if(feelAround == true && editedInput == "search wallet" || feelAround == true && editedInput == "open wallet"|| feelAround == true && editedInput == "look through wallet"){
+      let phoneText = "> Alright, I'll feel it out";
+      let phoneText2 = "> Oh! I think I left my phone in here!"
+      let acquiredText = "Aquired: Phone";
+      createEvent(phoneText,textHolder);
+      setTimeout(function(){createEvent(phoneText2,textHolder)},3000);
+      setTimeout(function(){createEventMotion(acquiredText,textHolder)},7000);
+    }
+    else if (feelAround == true && editedInput == "eat beans"|| feelAround == true && editedInput == "beans"){
+      let beanText = "> I don't have a can opener...";
+      let beanText2 = "> Well, I hope I'm not in here long enough that that matters";
+      createEvent(beanText,textHolder);
+      setTimeout(function(){createEvent(beanText2,textHolder)},3000);
+    }
+    else if(feelAround == true && editedInput == "drink water" || editedInput =="water"){
+      let waterText = "> Ok, if you really think so...";
+      let drinking = "> glug glug glug"
+      let waterText2 = "> I'm not feeling so good..."
+      let waterText3 = "> ...mom?"
+      let endingText = "> Type restart to start this story again or type back to go back to the title screen.";
+      createEvent(waterText,textHolder);
+      setTimeout(function(){createEventMotion(drinking,textHolder)},3000);
+      setTimeout(function(){createEvent(waterText2,textHolder)},7000);
+      setTimeout(function(){createEvent(waterText3,textHolder)},10000);
+      setTimeout(function(){displayEnding(foundEndings,textHolder)},13000);
+      if (foundEndings <= 5) {
+        foundEndings += 1;
+      }
+        setTimeout(function(){createEventMotion(endingText,textHolder)},15000);
+    }
+    //flavor responses below
     else if(editedInput == "look around" || editedInput == "look"){
       let text = "> I can't see anything! The room is literally pitch black";
       createEvent(text,textHolder);
@@ -92,6 +136,17 @@ function check(){
         let helpContent = "> Help? Um, I'm not sure what I can do, but good luck I guess!";
         createEvent(helpContent,textHolder);
     }
+    else if(editedInput == "how are you?"|| editedInput == "how are you"|| editedInput == "how r u" || editedInput == "how r u?"){
+      let flavor = "> I'm good!";
+      let flavor2 = "> Well, actually I'm not, but I guess I'm fine for the circumstances";
+      createEvent(flavor,textHolder);
+      setTimeout(function(){createEvent(flavor2,textHolder)},2000);
+    }
+    else if (editedInput == "cry"|| editedInput == "scream" || editedInput == "breakdown"|| editedInput == "freak out"){
+      let unhelpfulText = "> Um, I'm not sure if that would help"
+      createEvent(unhelpfulText,textHolder);
+    }
+    //functionality responses below
     else if(editedInput == "back"){
       window.location.href = "../";
       //this is essentially the back button lol
@@ -99,15 +154,9 @@ function check(){
     else if(editedInput == "restart"){
       textHolder.innerHTML = "";
       wait = false;
+      helpResponse = false;
       onloadText();
-      //restarts the game, but shouldn't restart the ending counter (still might tho)
-    }
-    else if(editedInput == "how are you?"|| editedInput == "how are you"){
-      let flavor = "> I'm good!";
-      let flavor2 = "> Well, actually I'm not, but I guess I'm fine for the circumstances";
-      createEvent(flavor,textHolder);
-      setTimeout(function(){createEvent(flavor2,textHolder)},2000);
-      // just some flavor text to get to know carol better
+      //restarts the game, but shouldn't restart the ending counter (untested)
     }
     else{
       let confusedText = "> Sorry, I didn't get that.";
@@ -155,7 +204,6 @@ function createEventMotion(txt,holder){
 
 //displays when ending is found
 function displayEnding(endings,holder){
-  endings++;
   let text = document.createElement('p');
   text.setAttribute('class','gameTextEnding');
   text.textContent = "<Ending " + endings.toString() + " of 5>";
